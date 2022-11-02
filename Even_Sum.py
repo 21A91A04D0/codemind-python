be43003b558e4128de0190842d68a7c38b541c19
21A91A04D0 <98007068+21A91A04D0@users.noreply.github.com>
2022-11-02 15:44:14 +0530

    n=int(input()) lst=list(map(int,input().split())) evens=[i for i in lst if i%2==0] print(sum(evens))

--- Even_Sum.py
+++ Even_Sum.py
@@ -1,7 +1,4 @@
 n=int(input())
-x=list(map(int,input().split()))
-sum=0
-for i in range(len(x)):
-    if x[i]%2==0:
-        sum=sum+x[i]
-print(sum)
+lst=list(map(int,input().split()))
+evens=[i for i in lst if i%2==0]
+print(sum(evens))
