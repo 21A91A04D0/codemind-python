b95fd75639f727c6407e1a80c231683bdc003ec4
21A91A04D0 <98007068+21A91A04D0@users.noreply.github.com>
2022-12-06 06:29:01 +0530

    def ugly_number(n):     while n%2==0:         n=n//2     if n==1:         return True     while n%3==0:         n=n//3     if n==1:         return True     while n%5==0:         n=n//5     if n==1:         return True
    n=int(input())
    if ugly_number(n):
        print("Ugly Number")
    else:
        print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,20 @@
+def ugly_number(n):
+    while n%2==0:
+        n=n//2
+    if n==1:
+        return True
+    while n%3==0:
+        n=n//3
+    if n==1:
+        return True
+    while n%5==0:
+        n=n//5
+    if n==1:
+        return True
+        
+        
+n=int(input())
+if ugly_number(n):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
