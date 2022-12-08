a40b4404613cfe501bef382dd8f5ba49c07f3a10
21A91A04D0 <98007068+21A91A04D0@users.noreply.github.com>
2022-12-08 06:33:47 +0530

    def fun(ch):     vowels='aeiouAEIOU'     cnt=0     for i in ch:         if i in vowels:             cnt+=1     return cnt s=input() k=s.split() a=[] for i in k:     a.append(fun(i)) t=min(a) ct=0 for i in a:     if i==t:         ct+=1 print(ct)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,19 @@
+def fun(ch):
+    vowels='aeiouAEIOU'
+    cnt=0
+    for i in ch:
+        if i in vowels:
+            cnt+=1
+    return cnt
+s=input()
+k=s.split()
+a=[]
+for i in k:
+    a.append(fun(i))
+t=min(a)
+ct=0
+for i in a:
+    if i==t:
+        ct+=1
+print(ct)
+    
