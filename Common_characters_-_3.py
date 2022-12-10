af841a93b127fe93a03e2265a5dddd461f0a15ba
21A91A04D0 <98007068+21A91A04D0@users.noreply.github.com>
2022-12-10 17:18:47 +0530

    def fun(c,k):     j=1     cnt=0     while j<len(k):         if c in k[j]:             cnt+=1         j+=1     return cnt s1=input() a1=s1.lower() k=a1.split() d=k[0] a=[] for i in d:     res=fun(i,k)     if res==len(k)-1:         a.append(i) if len(a)==0:     print("-1") else:     print(min(a))

---
+++ Common_characters_-_3.py
@@ -0,0 +1,23 @@
+def fun(c,k):
+    j=1
+    cnt=0
+    while j<len(k):
+        if c in k[j]:
+            cnt+=1
+        j+=1
+    return cnt
+s1=input()
+a1=s1.lower()
+k=a1.split()
+d=k[0]
+a=[]
+for i in d:
+    res=fun(i,k)
+    if res==len(k)-1:
+        a.append(i)
+if len(a)==0:
+    print("-1")
+else:
+    print(min(a))
+        
+    
